68c6c94e8b85c64b858c018bce11c17ebd3b91ad
Maximilian Stroh <Hisako1337@gmail.com>
2017-07-14 22:58:28 +0200

    simplified stopwords usage internally

--- src/lib/tools/stoplist.ts
+++ src/lib/tools/stoplist.ts
@@ -2,23 +2,14 @@
  * This module loads a big list of stopwords for various languages from the
  * json file in the `data` folder, and returns the specific list for a given
  * language as a `Set`.
+ * This module loads a language's stopwords array from predefined typescript
+ * files. For more languages, ensure that the `guess_language` function can
+ * recognize them, add their name mapping there, and create a new file with
+ * that name in the 'stopwords'-folder. There is a vast amount of source-data
+ * available for many languages in the top-level 'data'-folder.
  */
 
-// tslint:disable-next-line
-const stopwords = require('../../../data/stopwords.json')
-
-// see https://www.npmjs.com/package/stopwords-json for overview
-const languageMappings = {
-  dutch: 'nl',
-  english: 'en',
-  german: 'de',
-  italian: 'it',
-  portuguese: 'pt',
-  spanish: 'es',
-  swedish: 'sv',
-}
-
 export default function load(language: string): Set<string> {
-  const list: string[] = stopwords[languageMappings[language]]
+  const list: string[] = require('../stopwords/' + language).default
   return new Set(list)
 }
