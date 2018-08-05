e5b8647483130e75640954e87faea0e797d99560
Alex Apostol <newalex997@gmail.com>
2018-08-05 20:10:17 +0200

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,12 +5,17 @@ import guessLanguage from './tools/guess_language'
 const defaults: IOptions = {
   delimiters: ['\\s+'],
   language: 'english',
+  blockwords: [],
 }
 
 export default function process(text: string, opts?: IOptions): string[] {
   const options: IOptions = merge({}, defaults, opts)
   if (!opts || !opts.language) {
     options.language = guessLanguage(text)
+  }
+  
+  if (!opts || !opts.blockwords) {
+    options.blockwords = []
   }
   const params = merge(options, { corpus: text })
   const keywords = rake(params)
